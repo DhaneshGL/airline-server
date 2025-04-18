f94987dd4726955308c2e57367909a0b9e9b6b5a
DhaneshGL <dhanesh2094@gmail.com>
2025-04-18 13:26:00 +0000

    commit

---
+++ server/src/main/java/com/dhanesh/social/repositories/ReplyRepository.java
@@ -0,0 +1,9 @@
+package com.dhanesh.social.repositories;
+
+import com.dhanesh.social.models.Reply;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
+}
