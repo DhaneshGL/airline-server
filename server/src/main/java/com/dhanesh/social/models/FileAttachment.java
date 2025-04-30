9302ae2a3adfbcf904bc5315194f2bfa78889fd8
DhaneshGL <dhanesh2094@gmail.com>
2025-04-30 12:13:00 +0000

    commit

---
+++ server/src/main/java/com/dhanesh/social/models/FileAttachment.java
@@ -0,0 +1,15 @@
+package com.dhanesh.social.models;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class FileAttachment {
+    private String url;
+    private String type;  
+}
