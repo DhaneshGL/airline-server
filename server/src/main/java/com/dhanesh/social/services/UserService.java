291bf977cf19be7fb10f6b9922d0a730255a8c5f
DhaneshGL <dhanesh2094@gmail.com>
2025-05-04 12:54:00 +0000

    commit

---
+++ server/src/main/java/com/dhanesh/social/services/UserService.java
@@ -0,0 +1,44 @@
+package com.dhanesh.social.services;
+
+import com.dhanesh.social.models.User;
+import com.dhanesh.social.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public Boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
+}
